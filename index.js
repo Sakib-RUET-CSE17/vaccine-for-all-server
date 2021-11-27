6d7c9a714077e7815403fe1f62ada1a2838d687c
Sakib Hasan <hsakib8685@gmail.com>
2021-11-27 21:43:33 +0600

    heroku error resolve test

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID
-const cors = require('cors')
-const bodyParser = require('body-parser')
-require('dotenv').config()
+const ObjectID = require('mongodb').ObjectID;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(bodyParser.json())
